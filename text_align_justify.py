d8c601ba4ee8f8f6cfe2d871531643e2b2ed6581
pkbla <pkblasiak@gmail.com>
2021-02-21 13:01:26 +0100

    sentence is created but I need to add required amount of spaces between words and solution is incorrect when last word in given text should be not alone in last row

--- text_align_justify.py
+++ text_align_justify.py
@@ -1,6 +1,6 @@
-text = "If enabled (On) and the user hovers/ clicks/ taps over the information icon (i) next to the field name, displays the help text you enter in the provided text box. Maximum length: 140 characters"
+# text = "If enabled (On) and the user hovers/ clicks/ taps over the information icon (i) next to the field name, displays the help text you enter in the provided text box. Maximum length: 140 characters"
 
-# text = "If enabled (On) and the user hovers/ clicks/ taps over the information icon (i) next to the field name, displays the help text you enter in the provided text box. Maximum length: 140"
+text = "If enabled (On) and the user hovers/ clicks/ taps over the information icon (i) next to the field name, displays the help text you enter in the provided text box. Maximum length: 140"
 
 def divide_to_words(text):
     words = []
@@ -31,54 +31,6 @@ words = divide_to_words(text)
 print(words)
 
 
-# def justify(text, width):
-#     words = divide_to_words(text)
-#     lengths = show_lengths(words)
-#     lines = []
-#     i = 0
-#     while i+1 < len(lengths):
-#         sum = lengths[i]
-#         while sum <= width:
-#             sum = sum + 1 + lengths[i+1]
-#             i += 1
-#         sum = sum - 1 - lengths[i]
-#         lines.append(sum)
-#     lines.append(lengths[i])
-#     return lines
-
-
-# def justify(text, width):
-#     words = divide_to_words(text)
-#     lengths = show_lengths(words)
-#     lines_lengths = []
-#     first_last_word_in_line = []
-#     sentence = ''
-#     i = 0
-#     while i+1 < len(words):
-#         sum = lengths[i]
-#         a = i
-#         while sum <= width and i+1<len(words):
-#             sum = sum + 1 + lengths[i+1]
-#             i += 1
-#         sum = sum - 1 - lengths[i]
-#         z = i
-#         az = [a,z]
-#         lines_lengths.append(sum)
-#         first_last_word_in_line.append(az)
-#     lines_lengths.append(lengths[i])
-#     first_last_word_in_line.append(z)
-#     word_numbers = []
-#     # word_numbers.append(first_last_word_in_line[0][0])
-#     i = 0
-#     while i < len(first_last_word_in_line):
-#
-#         word_numbers.append(type(first_last_word_in_line[i]))
-#         word_numbers.append('//')
-#         i += 1
-#
-#     return lines_lengths, first_last_word_in_line, word_numbers
-
-
 def justify(text, width):
     words = divide_to_words(text)
     lengths = show_lengths(words)
@@ -104,6 +56,7 @@ def justify(text, width):
         for m in n:
             sentence = sentence + words[m] + ' '
         sentence = sentence + '\n'
+    sentence = sentence[0: -2]
     return lines_lengths, first_last_word_in_line, sentence
 
 
